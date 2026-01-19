22591db7ab7bb07d8ac7f7e9c00bda2bdf6e2fa6
tungaeric <tungaeric11@gmail.com>
2026-01-19 09:56:06 -1000

    Add files via upload

---
+++ NotificationService.java
@@ -0,0 +1,8 @@
+package projecgrade;
+
+public class NotificationService {
+
+    public void notifyStudent(String studentId, String message) {
+        System.out.println("Notification sent to Student " + studentId + ": " + message);
+    }
+}
